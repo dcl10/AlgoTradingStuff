4d933986d378f92e5cc4dd3ebc22ae2a2b2910e6
Daniel <daniel.lea@nottingham.ac.uk>
2019-02-01 18:28:52 +0000

    Expanded BaseHolding class

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -5,17 +5,6 @@ class Portfolio:
         self.holdings = {}
         self.pot = pot
 
-    def add_holdings(self, *args, **kwargs):
-        if args:
-            for arg in args:
-                self.holdings.update({arg.name: arg})
-        if kwargs:
-            self.holdings.update(**kwargs)
-
-    def remove_holding(self, *args):
-        for arg in args:
-            del self.holdings[arg]
-
     @property
     def current_value(self):
         return sum([h.balance for h in self.holdings])
@@ -24,12 +13,52 @@ class Portfolio:
     def returns(self):
         return self.current_value / self.pot
 
+    def add_holdings(self, *args, **kwargs):
+        if args:
+            for arg in args:
+                self.holdings.update({arg.name: arg})
+        if kwargs:
+            self.holdings.update(**kwargs)
+
+    def remove_holdings(self, *args):
+        for arg in args:
+            del self.holdings[arg]
+
     def allocate(self, holding, amount):
-        pass
+        self.holdings[holding].pot += amount
 
 
 class BaseHolding:
 
-    def __init__(self, name, pot=0.0):
+    def __init__(self, name, n_units=0):
         self.name = name
-        self.pot = pot
+        self.current_price = self.get_current_price()
+        self.n_units = n_units
+
+    @property
+    def balance(self):
+        return self.current_price * self.n_units
+
+    def get_current_price(self):
+        return 0.0  # TODO replace with call to quandl API
+
+    def buy(self, n_units=None, amount=None):
+        if n_units is not None:
+            self.n_units += n_units
+        elif amount is not None:
+            self.n_units += amount / self.current_price
+        else:
+            raise TypeError('You must specify either the number of units (n_units) or the amount (amount) '
+                            'that you wish to purchase.')
+
+    def sell(self, n_units=None, amount=None):
+        if n_units is not None:
+            self.n_units -= n_units
+        elif amount is not None:
+            self.n_units -= amount / self.current_price
+        else:
+            raise TypeError('You must specify either the number of units (n_units) or the amount (amount) '
+                            'that you wish to purchase.')
+
+    def update(self):
+        pass
