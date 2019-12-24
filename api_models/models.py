3d09a3d6c05bb293a37e793f59120515987e1ced
Daniel <lea.daniel@hotmail.co.uk>
2019-12-24 19:57:59 +0000

    started creating account class for oanda api

---
+++ api_models/models.py
@@ -0,0 +1,14 @@
+import requests
+
+
+class Account:
+
+    def __init__(self, **kwargse):
+        self.__dict__.update(kwargse)
+
+    def create_order(self, units, ):
+
+        pass
+
+    def cancel_order(self):
+        pass
