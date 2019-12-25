a511a86a5a9ceebca45ff2378a3a5ce346d47f2a
Daniel <lea.daniel@hotmail.co.uk>
2019-12-25 17:05:22 +0000

    started writing unittests for Account methods

--- api_models/models.py
+++ api_models/models.py
@@ -3,7 +3,8 @@ import requests
 
 class Account:
 
-    def __init__(self, **kwargse):
+    def __init__(self, api_key, **kwargse):
+        self.api_key = api_key
         self.__dict__.update(kwargse)
 
     def create_order(self, units, ):
@@ -12,3 +13,14 @@ class Account:
 
     def cancel_order(self):
         pass
+
+
+def get_accounts(api_key):
+    response = requests.get(f'https://api-fxpractice.oanda.com/v3/accounts/',
+                            headers={'Authorization': f'Bearer {api_key}'})
+    pass
+
+
+def get_account(account_id: str, api_key: str):
+    response = requests.get(f'https://api-fxpractice.oanda.com/v3/accounts/{account_id}')
+    pass
---
+++ api_models/test_models.py
@@ -0,0 +1,38 @@
+import unittest
+import configparser
+from api_models.models import Account, get_accounts, get_account
+
+
+class TestAccount(unittest.TestCase):
+
+    def setUp(self) -> None:
+        parser = configparser.ConfigParser()
+        parser.read('./oanda_config.txt')
+        self.api_key = parser['oanda'].get('api_key')
+
+    def test_create_order(self):
+        account = Account(self.api_key, )
+        pass
+
+
+class TestStaticMethods(unittest.TestCase):
+
+    def setUp(self) -> None:
+        parser = configparser.ConfigParser()
+        parser.read('./oanda_config.txt')
+        self.api_key = parser['oanda'].get('api_key')
+        self.account = parser['oanda'].get('primary_account')
+
+    def test_get_accounts(self):
+        accounts = get_accounts(self.api_key)
+        self.assertIsInstance(accounts, list)
+        self.assertIsInstance(accounts[0], dict)
+        bad_key = get_accounts('jfjwjfwjfjwfj')
+        self.assertEqual(bad_key, None)
+
+    def test_get_account(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
