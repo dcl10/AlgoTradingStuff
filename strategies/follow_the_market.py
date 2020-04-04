05c070e1a916291b74115d9584613464573e2f5a
Daniel <lea.daniel@hotmail.co.uk>
2020-04-04 19:59:27 +0100

    Added command line args to follow_the_market.pu

--- strategies/follow_the_market.py
+++ strategies/follow_the_market.py
@@ -1,19 +1,31 @@
 import sys
 import configparser
+import argparse
+import datetime as dt
 from api_models.models import get_account, Account
 from backtest.backtest import BackTester
 
-oanda_config = sys.argv[1]
-start_date = sys.argv[2]
-end_date = sys.argv[3]
-instrument = sys.argv[4]
+a_parser = argparse.ArgumentParser()
+a_parser.add_argument('config', help='configuration file')
+a_parser.add_argument('-s', '--start', dest='start', help='start date for the back test period',
+                      default=(dt.datetime.today() - dt.timedelta(days=60)).strftime('%Y-%m-%d %H:%M:%S'))
+a_parser.add_argument('-e', '--end', dest='end', help='end date for the back test period',
+                      default=dt.datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+a_parser.add_argument('-i', '--instrument', dest='instrument', help='the instrument to back test', default='GBP_USD')
 
-parser = configparser.ConfigParser()
-parser.read(oanda_config)
+args = a_parser.parse_args(sys.argv[1:])
 
-api_key = parser.get('oanda', 'api_key')
-account_id = parser.get('oanda', 'primary_account')
-base_url = parser.get('oanda', 'base_url')
+oanda_config = args.config
+start_date = args.start
+end_date = args.end
+instrument = args.instrument
+
+c_parser = configparser.ConfigParser()
+c_parser.read(oanda_config)
+
+api_key = c_parser.get('oanda', 'api_key')
+account_id = c_parser.get('oanda', 'primary_account')
+base_url = c_parser.get('oanda', 'base_url')
 
 oanda_account = get_account(base_url=base_url, api_key=api_key, account_id=account_id)
 account = Account(base_url=base_url, account_id=account_id, api_key=api_key, **oanda_account)
