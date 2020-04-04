f82458c5d8e8615aae57b89f5a344feac79e714b
Daniel <lea.daniel@hotmail.co.uk>
2020-04-04 13:00:43 +0100

    Altered BackTester class to take a list of instructions and a list of prices; added docstrings to BackTester; wrote unit tests for buying and selling - passing

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -3,43 +3,43 @@ class BackTester:
     This class takes a set of buy/sell instructions and tests if they would make a profit in a given time range.
     """
 
-    def __init__(self, balance: float, start: str, end: str, instructions: list, margin: float = 1.0):
+    def __init__(self, balance: float, instructions: list, prices: list, margin: float = 1.0):
         """
 
-        :param balance:
-        :param start:
-        :param end:
-        :param instructions:
-        :param margin:
+        :param balance: The starting amount for the back-test
+        :param instructions: A list of either booleans or ints in {0,1} where True/1 means buy and False/0 means sell
+        :param prices: A list of prices at which to buy or units
+        :param margin: The proportion of your balance you are willing to use in the transaction
         """
         self.balance = balance
-        self.start = start
-        self.end = end
         self.instructions = instructions
         self.margin = margin
+        self.prices = prices
         self.result = balance
 
-    def _sell(self):
+    def _sell(self, price: float):
         """
         Sell units
+        :param price: the price at which to sell
         :return:
         """
-        self.result += (self.margin * self.result)
+        self.result += (self.margin * self.result) * price
 
-    def _buy(self):
+    def _buy(self, price: float):
         """
         Buy units
+        :param: the price at to buy
         :return:
         """
-        self.result -= (self.margin * self.result)
+        self.result -= (self.margin * self.result) * price
 
     def run(self):
         """
         Runs the instructions given and determines total profit
         :return:
         """
-        for ins in self.instructions:
+        for ins, pri in zip(self.instructions, self.prices):
             if ins:
-                self._buy()
+                self._buy(pri)
             else:
-                self._sell()
+                self._sell(pri)
