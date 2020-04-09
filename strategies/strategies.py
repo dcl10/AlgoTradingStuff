c3846bc1e25933e6bc8a4d8962840119a9038fab
Daniel Lea <dan.carr.lea@gmail.com>
2020-04-09 17:05:26 +0100

    defined constructor for BaseStrategy and a timedelta dict for operating the steps forward through time

--- strategies/strategies.py
+++ strategies/strategies.py
@@ -4,7 +4,24 @@ from abc import ABC, abstractmethod
 from api_models.models import Account
 
 
+def vals_from_candles(candles):
+    vals = []
+    for c in candles:
+        if 'mid' in c.keys():
+            vals.append(float(c.get('mid').get('c', 0.0)))
+        elif 'bid' in c.keys():
+            vals.append(float(c.get('bid').get('c', 0.0)))
+        elif 'ask' in c.keys():
+            vals.append(float(c.get('ask').get('c', 0.0)))
+        else:
+            vals.append(0.0)
+    return vals
+
+
 class BaseStrategy(ABC):
+
+    deltas = {'H1': dt.timedelta(hours=1), 'H6': dt.timedelta(hours=6), 'H12': dt.timedelta(hours=12),
+              'D': dt.timedelta(days=1), 'W': dt.timedelta(weeks=1)}
 
     def __init__(self, account: Account, granularity: str = 'D',
                  start_date: str = (dt.datetime.today() - dt.timedelta(days=60)).strftime('%Y-%m-%d %H:%M:%S')):
@@ -19,19 +36,6 @@ class BaseStrategy(ABC):
     @abstractmethod
     def generate_instructions(self):
         pass
-
-    def _vals_from_candles(self, candles):
-        vals = []
-        for c in candles:
-            if 'mid' in c.keys():
-                vals.append(float(c.get('mid').get('c', 0.0)))
-            elif 'bid' in c.keys():
-                vals.append(float(c.get('bid').get('c', 0.0)))
-            elif 'ask' in c.keys():
-                vals.append(float(c.get('ask').get('c', 0.0)))
-            else:
-                vals.append(0.0)
-        return vals
 
 
 class FollowMarketStrategy(BaseStrategy):
